ed5d4baee213ef8de846d4b6e537e223f43e1c4a
Paco Morcillo Vizuete <pacomorcillovizuete@gmail.com>
2022-07-11 01:42:51 +0200

    fix.

--- sequential_jobs/job0.sh
+++ sequential_jobs/job0.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 ##----------------------- Start job description -----------------------
-#SBATCH --partition=standard-gpu
-#SBATCH --job-name=job8_011
+#SBATCH --partition=standard
+#SBATCH --job-name=job1_000
 #SBATCH --ntasks=1
 #SBATCH --time=02-00:00:00
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=francisco.morcillo.vizuete@alumnos.upm.es
 ##------------------------ End job description ------------------------
 
-module purge && module load Python/3.8.6-GCCcore-10.2.0 && module load CUDA/11.1.1-GCC-10.2.0
+module purge && module load Python/3.8.6-GCCcore-10.2.0
 
 . ../../../t949996/allennlp/bin/activate
 
+export OMP_NUM_THREADS="${SLURM_CPUS_PER_TASK}"
+
 for i in $(seq 0 29); do
- srun allennlp train FRMiner2/config3.json -s FRMiner2/out-a/out-8$i/ -f --include-package FRMiner2
+ srun allennlp train FRMiner2/config.json -s FRMiner2/out-a/out-1$i/ -f --include-package FRMiner2
 done
--- sequential_jobs/job1.sh
+++ sequential_jobs/job1.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 ##----------------------- Start job description -----------------------
-#SBATCH --partition=standard
-#SBATCH --job-name=job1_000
+#SBATCH --partition=standard-gpu
+#SBATCH --job-name=job2_001
 #SBATCH --ntasks=1
 #SBATCH --time=02-00:00:00
 #SBATCH --mem=32G
-#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:v100:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=francisco.morcillo.vizuete@alumnos.upm.es
 ##------------------------ End job description ------------------------
 
-module purge && module load Python/3.8.6-GCCcore-10.2.0
+module purge && module load Python/3.8.6-GCCcore-10.2.0 && module load CUDA/11.1.1-GCC-10.2.0
 
 . ../../../t949996/allennlp/bin/activate
 
-export OMP_NUM_THREADS="${SLURM_CPUS_PER_TASK}"
-
 for i in $(seq 0 29); do
- srun allennlp train FRMiner2/config.json -s FRMiner2/out-a/out-1$i/ -f --include-package FRMiner2
+ srun allennlp train FRMiner2/config1.json -s FRMiner2/out-a/out-2$i/ -f --include-package FRMiner2
 done
